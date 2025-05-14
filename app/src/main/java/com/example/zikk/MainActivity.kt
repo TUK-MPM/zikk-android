edfbcd987cc3e1e546feb75b5c9969bcec0e16ce
JungWoo Song <jsmac7014@gmail.com>
2025-05-14 19:56:36 +0900

    feat: 예시용 버튼 클릭 시 네트워크 요청

--- app/src/main/java/com/example/zikk/MainActivity.kt
+++ app/src/main/java/com/example/zikk/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.example.zikk
 
 import android.os.Bundle
+import android.telecom.Call
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.zikk.databinding.ActivityMainBinding
+import com.example.zikk.network.RetrofitClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
+import okhttp3.Response
+import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.test.setOnClickListener {
+            getTodos()
+        }
+    }
+
+    private fun getTodos() {
+        lifecycleScope.launch {
+            try {
+                val response = RetrofitClient.apiService.getTodos()
+                if (response.isSuccessful) {
+                    val todos = response.body()
+                    Log.d("테스트", todos?.get(0)?.title.toString())
+                    Log.d("테스트", todos?.get(0)?.userId.toString())
+                    Log.d("테스트", todos?.get(0)?.id.toString())
+                    Log.d("테스트", todos?.get(0)?.completed.toString())
+                } else {
+                    Toast.makeText(this@MainActivity, "서버 오류: ${response.code()}", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                e.localizedMessage?.let { Log.d("테스트 요청", it) }
+                Toast.makeText(this@MainActivity, "요청 실패: ${e.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
