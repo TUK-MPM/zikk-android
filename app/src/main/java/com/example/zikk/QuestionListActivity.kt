84700082e1392545dca110716d6d73fb43752864
김서연 <seoyoun1004@gmail.com>
2025-05-31 12:41:43 +0900

    feat: QuestionListActivity Design

---
+++ app/src/main/java/com/example/zikk/QuestionListActivity.kt
@@ -0,0 +1,27 @@
+package com.example.zikk
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class QuestionListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_question_list)
+
+        val spinner: Spinner = findViewById(R.id.mySpinner)
+        val items = arrayOf("최신순", "오래된순")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        spinner.adapter = adapter
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
