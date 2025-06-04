1e9be58c83e04d792fbda34350c21997e3c7e339
JungWoo Song <jsmac7014@gmail.com>
2025-06-04 13:33:30 +0900

    chore: base url 변경

--- app/src/main/java/com/example/zikk/network/RetrofitClient.kt
+++ app/src/main/java/com/example/zikk/network/RetrofitClient.kt
@@ -6,7 +6,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     // TODO
     // 백엔드 개발 작업 들어가면 BASE_URL 변경할 것
-    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+//    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    private const val BASE_URL = "http://3.39.234.193:8080/api/"
+
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
