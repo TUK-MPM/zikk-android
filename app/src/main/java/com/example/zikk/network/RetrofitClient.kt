467d75557d24c2363165fa3d4323ed94943b63b7
JungWoo Song <jsmac7014@gmail.com>
2025-06-04 13:56:46 +0900

    chore: TODO 완료 후 삭제

--- app/src/main/java/com/example/zikk/network/RetrofitClient.kt
+++ app/src/main/java/com/example/zikk/network/RetrofitClient.kt
@@ -4,9 +4,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    // TODO
-    // 백엔드 개발 작업 들어가면 BASE_URL 변경할 것
-//    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
     private const val BASE_URL = "http://3.39.234.193:8080/api/"
 
 
