51610b850bd145e882f9d09fbd6b4f470d73ccd6
blueTino <135584785+blueTino@users.noreply.github.com>
2025-06-07 20:55:26 +0900

    feat: 문의 상세 조회 API 연동 및 완성

--- app/src/main/java/com/example/zikk/network/RetrofitClient.kt
+++ app/src/main/java/com/example/zikk/network/RetrofitClient.kt
@@ -6,7 +6,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     private const val BASE_URL = "http://3.39.234.193:8080/api/"
 
-
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())  // JSON -> 객체 변환을 위한 Gson 사용
